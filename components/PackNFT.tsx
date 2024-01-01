018f183788cc914a0bc1dea853aa334870cbf449
Yash <67926590+Yash094@users.noreply.github.com>
2024-01-02 03:21:43 +0530

    Fix: Only display active listings on the shop page

--- components/PackNFT.tsx
+++ components/PackNFT.tsx
@@ -1,73 +1,86 @@
 import { MARKETPLACE_ADDRESS, PACK_ADDRESS } from "../const/addresses";
-import { MediaRenderer, Web3Button, useAddress, useContract, useDirectListings, useNFT } from "@thirdweb-dev/react";
+import {
+  MediaRenderer,
+  Web3Button,
+  useAddress,
+  useContract,
+  useDirectListing,
+  useNFT,
+} from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 type Props = {
-    contractAddress: string;
-    tokenId: any;
+  listingId: any;
+  tokenId: any;
 };
 
-export const PackNFTCard = ({ contractAddress, tokenId }: Props) => {
-    const address = useAddress();
+export const PackNFTCard = ({ listingId, tokenId }: Props) => {
+  const address = useAddress();
 
-    const { contract: marketplace, isLoading: loadingMarketplace } = useContract(MARKETPLACE_ADDRESS, "marketplace-v3");
-    const { contract: packContract } = useContract(contractAddress);
-    const { data: packNFT, isLoading: loadingNFT } = useNFT(packContract, tokenId);
+  const { contract: marketplace, isLoading: loadingMarketplace } = useContract(
+    MARKETPLACE_ADDRESS,
+    "marketplace-v3"
+  );
+  const { contract: packContract } = useContract(PACK_ADDRESS);
+  const { data: packNFT, isLoading: loadingNFT } = useNFT(
+    packContract,
+    tokenId
+  );
 
+  const { data: packListings, isLoading: loadingPackListings } =
+    useDirectListing(marketplace, listingId);
+  console.log("Pack Listings: ", packListings);
 
-    const { data: packListings, isLoading: loadingPackListings } = 
-    useDirectListings(
-        marketplace,
-        {
-            tokenContract: PACK_ADDRESS,
-        }
-    );
-    console.log("Pack Listings: ", packListings);
+  async function buyPack() {
+    let txResult;
 
-    async function buyPack() {
-        let txResult;
+    if (packListings) {
+      txResult = await marketplace?.directListings.buyFromListing(
+        packListings.id,
+        1
+      );
+    } else {
+      throw new Error("No valid listing found");
+    }
 
-        if (packListings?.[tokenId]) {
-            txResult = await marketplace?.directListings.buyFromListing(
-                packListings[tokenId].id,
-                1
-            )
-        } else {
-            throw new Error("No valid listing found");
-        }
-            
-        return txResult;
-    };
+    return txResult;
+  }
 
-    return (
-        <div className={styles.packCard}>
-            {!loadingNFT && !loadingPackListings ? (
-                <div className={styles.shopPack}>
-                    <div>
-                        <MediaRenderer
-                            src={packNFT?.metadata.image}
-                            width="80%"
-                            height="100%"
-                        />
-                    </div>
-                    <div className={styles.packInfo}>
-                        <h3>{packNFT?.metadata.name}</h3>
-                        
-                        <p>Cost: {packListings![tokenId].currencyValuePerToken.displayValue} {` ` + packListings![tokenId].currencyValuePerToken.symbol}</p>
-                        <p>Supply: {packListings![tokenId].quantity}</p>
-                        {!address ? (
-                            <p>Login to buy</p>
-                        ) : (
-                            <Web3Button
-                            contractAddress={MARKETPLACE_ADDRESS}
-                            action={() => buyPack()}
-                            >Buy Pack</Web3Button>
-                        )}
-                    </div>
-                </div>
+  return (
+    <div className={styles.packCard}>
+      {!loadingNFT && !loadingPackListings ? (
+        <div className={styles.shopPack}>
+          <div>
+            <MediaRenderer
+              src={packNFT?.metadata.image}
+              width="80%"
+              height="100%"
+            />
+          </div>
+          <div className={styles.packInfo}>
+            <h3>{packNFT?.metadata.name}</h3>
+
+            <p>
+              Cost:{" "}
+              {packListings && packListings.currencyValuePerToken.displayValue}{" "}
+              {packListings && ` ` + packListings.currencyValuePerToken.symbol}
+            </p>
+            <p>Supply: {packListings && packListings.quantity}</p>
+            {!address ? (
+              <p>Login to buy</p>
             ) : (
-                <p>Loading...</p>
+              <Web3Button
+                contractAddress={MARKETPLACE_ADDRESS}
+                action={() => buyPack()}
+              >
+                Buy Pack
+              </Web3Button>
             )}
+          </div>
         </div>
-    )
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,42 +1,34 @@
-import { useContract, useDirectListings } from "@thirdweb-dev/react";
+import { useContract, useValidDirectListings } from "@thirdweb-dev/react";
 import { MARKETPLACE_ADDRESS, PACK_ADDRESS } from "../const/addresses";
 import styles from "../styles/Home.module.css";
 import { PackNFTCard } from "../components/PackNFT";
 
 export default function Shop() {
-    const {
-        contract: marketplace,
-        isLoading: loadingMarketplace,
-    } = useContract(MARKETPLACE_ADDRESS, "marketplace-v3");
+  const { contract: marketplace, isLoading: loadingMarketplace } = useContract(
+    MARKETPLACE_ADDRESS,
+    "marketplace-v3"
+  );
 
-    const {
-        data: directListings,
-        isLoading: loadingDirectListings,
-    } = useDirectListings(
-        marketplace,
-        {
-            tokenContract: PACK_ADDRESS,
-        }
-    );
-    console.log("DirectPack", directListings);
+  const { data: directListings, isLoading: loadingDirectListings } =
+    useValidDirectListings(marketplace, {
+      tokenContract: PACK_ADDRESS,
+    });
+  console.log("DirectPack", directListings);
 
-    return (
-        <div className={styles.container}>
-            <h1>Shop Packs</h1>
-            <div className={styles.grid}>
-                {!loadingDirectListings ? (
-                    directListings?.map((listing, index) => (
-                        <div key={index}>
-                            <PackNFTCard
-                                contractAddress={listing.assetContractAddress}
-                                tokenId={listing.tokenId}
-                            />
-                        </div>
-                    ))
-                ) : (
-                    <p>Loading...</p>
-                )}
+  return (
+    <div className={styles.container}>
+      <h1>Shop Packs</h1>
+      <div className={styles.grid}>
+        {!loadingDirectListings ? (
+          directListings?.map((listing, index) => (
+            <div key={index}>
+              <PackNFTCard tokenId={listing.asset.id} listingId={listing.id} />
             </div>
-        </div>
-    )
-};
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+}
